3792dff5a93af5d1429816b532d56dbef1d0ad6c
young_jerry <494478066@qq.com>
2020-06-18 02:18:56 +0800

    测试

---
+++ iiiiii/iiiiii/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  iiiiii
+//
+//  Created by young_jerry on 2020/6/18.
+//  Copyright © 2020 young_jerry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
